3af9f367ec5b97c40cc6dcd2ac735015772ed64a
TryaNever <alexispernette@outlook.fr>
2025-11-28 11:40:25 +0100

    Patch H1

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,7 +9,6 @@ export default function Home() {
         <title>MySocialApp</title>
       </Helmet>
       <div className="bg-gray-50 min-h-screen text-center">
-        <h1> React + Vite + Express + Mysql + Docker</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-6 ">
           <PostList key={1} />
         </div>
