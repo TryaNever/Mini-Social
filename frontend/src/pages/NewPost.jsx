e2f17142241e9330d80043b2031fbcf0d30ded06
TryaNever <alexispernette@outlook.fr>
2025-11-28 11:12:17 +0100

    Add Message de devellopement

--- frontend/src/pages/NewPost.jsx
+++ frontend/src/pages/NewPost.jsx
@@ -8,6 +8,7 @@ export default function NewPost() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
       <div className="bg-white shadow-md rounded-xl p-6 max-w-md w-full text-center">
+        <p>Cette fonctionnalité est en cours de développement, désolé !</p>
         {currentStep === 1 && (
           <PostChooseModal
             setNewUrl={setCurrentUrl}
